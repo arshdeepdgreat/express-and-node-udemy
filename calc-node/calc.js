bc739bf418bd25efd878ec7f48258454c33e2330
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-01-28 12:57:19 +0530

    Added bmi calc function

--- calc-node/calc.js
+++ calc-node/calc.js
@@ -8,14 +8,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
 })
-app.get("/bmi",function(req,res){
-    res.sendFile(__dirname+"/bmiCalc.html")
-})
 app.post("/",function(req,res){
     var num1=Number(req.body.num1);
     var num2=Number(req.body.num2);
     var ans=num1+num2;
     res.send("Answer is : "+ ans)
+})
+
+
+app.get("/bmi",function(req,res){
+    res.sendFile(__dirname+"/bmiCalc.html")
+})
+app.post("/bmi",function(req,res){
+    var num1=Number(req.body.height);
+    var num2=Number(req.body.weight);
+    var ans=num2/(num1*num1);
+    res.send("BMI is : "+ ans)
 })
 
 app.listen(3000,function(){
