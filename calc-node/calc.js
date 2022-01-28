c08f57af98eb1da0e7fff3deb59f8ddb6bacf91c
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-01-28 12:42:47 +0530

    basic posting

--- calc-node/calc.js
+++ calc-node/calc.js
@@ -7,6 +7,10 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
 })
 
+app.post("/",function(req,res){
+    res.send("Thanks for posting")
+})
+
 app.listen(3000,function(){
     console.log("Server is running at 3000")
 })
