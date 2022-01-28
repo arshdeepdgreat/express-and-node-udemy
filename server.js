256d14951b1980d81b2a076e6b38e52b69914d53
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-01-28 12:07:36 +0530

    basic routing

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
   res.send('Contact me at arshdeepdgreat@gmail.com')
 })
+app.get('/about', (req, res) => {
+  res.send('Hey im arshdeep and i am learning nodejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
