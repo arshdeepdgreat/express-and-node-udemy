1bfc3dde8a53910b1b66023e65879eee6ae0e59a
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-01-28 12:03:56 +0530

    basic routing

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/contact', (req, res) => {
+  res.send('Contact me at arshdeepdgreat@gmail.com')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
