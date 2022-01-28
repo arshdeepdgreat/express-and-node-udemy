310dd57afce7d4d7052d2e5b100ceac9c72941d3
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-01-28 12:50:17 +0530

    calc posting and finished

--- calc-node/calc.js
+++ calc-node/calc.js
@@ -10,7 +10,10 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    console.log(req.body)
+    var num1=Number(req.body.num1);
+    var num2=Number(req.body.num2);
+    var ans=num1+num2;
+    res.send("Answer is : "+ ans)
 })
 
 app.listen(3000,function(){
